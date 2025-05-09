0e798f5a021e4ebaac9cc8d59ede5f95a4f1cf77
Shaun-MC <shaunmcushman@gmail.com>
2025-05-09 15:47:59 -0700

    remove unnecesary repo function

--- src/main/java/com/windowbutlers/backend/repository/JobRepo.java
+++ src/main/java/com/windowbutlers/backend/repository/JobRepo.java
@@ -1,23 +1,9 @@
 package com.windowbutlers.backend.repository;
 
-import com.windowbutlers.backend.enums.JobTitles;
 import com.windowbutlers.backend.entity.Jobs;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import java.sql.Date;
-import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface JobRepo extends JpaRepository<Jobs, UUID> {
-    
-    // Doesn't require a join table
-    @Query("SELECT j.id FROM Job j WHERE j.home.id = :homeID AND j.title = :title AND j.dateStarted = :dateStarted")
-    Optional<Integer> findByHomeIdAndTitleAndDateStarted(
-        @Param("homeID") Integer homeID,
-        @Param("title") JobTitles title,
-        @Param("dateStarted") Date dateStarted
-    );
-}
+public interface JobRepo extends JpaRepository<Jobs, UUID> {}
