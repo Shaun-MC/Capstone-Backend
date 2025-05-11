870e53647f8e5fcbd0bc87729e3075be699d0d26
Shaun-MC <shaunmcushman@gmail.com>
2025-05-10 20:21:18 -0700

    aligns the variable in the request objects to the tables properties (not nullable)

--- src/main/java/com/windowbutlers/backend/dto/PhoneNumberUpdateRequest.java
+++ src/main/java/com/windowbutlers/backend/dto/PhoneNumberUpdateRequest.java
@@ -1,12 +1,12 @@
 package com.windowbutlers.backend.dto;
 
 import com.windowbutlers.backend.validation.ValidPhoneNumber;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public class PhoneNumberUpdateRequest {
 
-    @NotBlank(message = "Phone number is required")
+    @NotNull(message = "Phone number is required")
     @ValidPhoneNumber(message = "Phone number is in an invalid format")
     @Size(min = 12, max = 12, message = "Phone number must 12 characters. EX: '123-456-7890'")
     private String phoneNumber;
