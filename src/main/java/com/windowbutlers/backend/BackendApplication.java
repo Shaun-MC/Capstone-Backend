92ba6f48816f91ef2e87e702468c7e9c06f116bb
Shaun-MC <shaunmcushman@gmail.com>
2025-05-11 13:01:03 -0700

    added future update update optimization note

--- src/main/java/com/windowbutlers/backend/BackendApplication.java
+++ src/main/java/com/windowbutlers/backend/BackendApplication.java
@@ -3,6 +3,12 @@ package com.windowbutlers.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Possible Optimization for all Update requests:
+/*
+ * You can use a custom repository method or a JPQL (Java Persistence Query Language) to update query to directly update the [ANY] field for the given
+ * ID. This avoids loading the entire Job entity into memory, which can improve performance, especially if the Job entity is large.
+ */
+
 @SpringBootApplication
 public class BackendApplication {
 
